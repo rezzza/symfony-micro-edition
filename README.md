20b07dccd44ecbaa55b5d0c3f853236ef241534e
Timothée Barray <tim@amicalement-web.net>
2016-04-22 17:06:36 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Symfony micro edition
+
+Based on micro kernel and reactphp, no bundle are embedded unless third party ones.
+
+## Default requirements
+
+Have a look on the [composer.json](composer.json#L15)
+
+## Customization
+
+1. Add your own [namespace in composer.json](composer.json#L10)
---
+++ app/AppKernel.php
@@ -0,0 +1,56 @@
+<?php
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+
+class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles()
+    {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new League\Tactician\Bundle\TacticianBundle(),
+        ];
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+        }
+
+        return $bundles;
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->import(__DIR__.'/config/routing.yml');
+    }
+
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+        $c->addCompilerPass(new LoadValidationFile([
+            '/config/validation.yml'
+        ]));
+        $loader->load(__DIR__.'/services/');
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+}
